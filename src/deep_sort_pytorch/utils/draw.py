49c631ca81f1c528e978d7bd14cd500083536da9
Keeron <keeron9@gmail.com>
2025-05-22 09:32:23 +0200

    Update draw.py
    fixed mistake

--- src/deep_sort_pytorch/utils/draw.py
+++ src/deep_sort_pytorch/utils/draw.py
@@ -29,8 +29,6 @@ def draw_boxes(img, bbox, names=None, identities=None, masks=None, offset=(0, 0)
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 0.5
     thickness = 1
-    t_size = cv2.getTextSize(label, font, font_scale, thickness)[0]
-    #t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
     for i, box in enumerate(bbox):
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -44,6 +42,8 @@ def draw_boxes(img, bbox, names=None, identities=None, masks=None, offset=(0, 0)
         if masks is not None:
             mask = masks[i]
             img = draw_masks(img, mask, color)
+        t_size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+        #t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
         cv2.rectangle(img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
         cv2.putText(img, label, (x1, y1 + t_size[1] + 4), font, font_scale, [255, 255, 255], thickness)
