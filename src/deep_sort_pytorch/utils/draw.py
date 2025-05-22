fdbb21ad42e46c144313c11861e5a7d2d243c4f6
Keeron <keeron9@gmail.com>
2025-05-22 09:23:38 +0200

    Update draw.py
    Made draw_boxes text smaller

--- src/deep_sort_pytorch/utils/draw.py
+++ src/deep_sort_pytorch/utils/draw.py
@@ -26,6 +26,11 @@ def draw_masks(image, mask, color, thresh: float = 0.7, alpha: float = 0.5):
 
 
 def draw_boxes(img, bbox, names=None, identities=None, masks=None, offset=(0, 0)):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    thickness = 1
+    t_size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+    #t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
     for i, box in enumerate(bbox):
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -36,13 +41,12 @@ def draw_boxes(img, bbox, names=None, identities=None, masks=None, offset=(0, 0)
         id = int(identities[i]) if identities is not None else 0
         color = compute_color_for_labels(id)
         label = '{:}{:d}'.format(names[i], id)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
         if masks is not None:
             mask = masks[i]
             img = draw_masks(img, mask, color)
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
         cv2.rectangle(img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
-        cv2.putText(img, label, (x1, y1 + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        cv2.putText(img, label, (x1, y1 + t_size[1] + 4), font, font_scale, [255, 255, 255], thickness)
     return img
 
 
